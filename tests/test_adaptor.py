a99c4a800d6f1d755d49dc3e8f48f0c157acd5fe
Gordon Watts <gwatts@uw.edu>
2024-12-08 16:50:16 -0500

    Implementing General Design (#2)
    * First go at how we expect things to look
    
    * Update comments
    
    * Improve formatting
    
    * Increase line length
    
    * Line length change

--- tests/test_adaptor.py
+++ tests/test_adaptor.py
@@ -1,6 +1,36 @@
-from servicex_local.adaptor import SXAdaptor_WSL2
+import pytest
+from servicex_local import ScienceWSL2, LocalXAOD, SXLocalAdaptor
+from servicex import query as q, deliver, dataset
 
 
-def test_adaptor():
-    adaptor = SXAdaptor_WSL2()
-    assert adaptor is not None
+@pytest.mark.skip(reason="This integration test is not ready to run")
+def test_adaptor_xaod_wsl2():
+    codegen = LocalXAOD()
+    science_runner = ScienceWSL2("atlas_al9", "22.2.107")
+    adaptor = SXLocalAdaptor(codegen, science_runner)
+
+    # The simple query, take straight from the example in the documentation.
+    query = q.FuncADL_ATLASr22()  # type: ignore
+    jets_per_event = query.Select(lambda e: e.Jets("AnalysisJets"))
+    jet_info_per_event = jets_per_event.Select(
+        lambda jets: {
+            "pt": jets.Select(lambda j: j.pt()),
+            "eta": jets.Select(lambda j: j.eta()),
+        }
+    )
+
+    spec = {
+        "Sample": [
+            {
+                "Name": "func_adl_xAOD_simple",
+                "Dataset": dataset.FileList(
+                    [
+                        "tests/test.root",  # noqa: E501
+                    ]
+                ),
+                "Query": jet_info_per_event,
+            }
+        ]
+    }
+    files = deliver(spec, servicex_name="servicex-uc-af", sx_adaptor=adaptor)
+    assert files is not None, "No files returned from deliver! Internal error"
