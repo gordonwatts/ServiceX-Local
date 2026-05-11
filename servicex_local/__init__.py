3764d290704b2307985702a80ea7aa1c6554d584
Roger Janusiak <roger22011@gmail.com>
2026-05-11 11:01:17 -0700

    Removed deliver from available functions.

--- servicex_local/__init__.py
+++ servicex_local/__init__.py
@@ -2,6 +2,6 @@ from .science_images import WSL2ScienceImage, DockerScienceImage  # noqa: F401
 from .science_images import SingularityScienceImage  # noqa: F401
 from .codegen import LocalXAODCodegen, DockerCodegen  # noqa: F401
 from .adaptor import SXLocalAdaptor  # noqa: F401
-from .deliver import local_deliver, deliver  # noqa: F401
+from .deliver import local_deliver  # noqa: F401
 from .configurations import xAODConfig  # noqa: F401
 from .utils import install_sx_local, Platform  # noqa: F401
